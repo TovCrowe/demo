d0b37f5a180a65fdb39599bace6f4863902b576f
TovCrowe <manuelxdvic@gmail.com>
2024-01-03 15:07:11 -0600

    userController, userservice and userRepository added

--- demo/src/main/java/com/example/demo/Services/UserService.java
+++ demo/src/main/java/com/example/demo/Services/UserService.java
@@ -23,7 +23,7 @@ public class UserService {
             userRepository.createUserQuery(user.getName(), user.getEmail(), user.getPassword());
         }
         public void editAllUserService(UserModel userEdit, Long id){
-            userRepository.editAlluserQuery(userEdit.getName(), userEdit.getEmail(), userEdit.getPassword(), id);
+            userRepository.editAllUserQuery(userEdit.getName(), userEdit.getEmail(), userEdit.getPassword(), id);
 
         }
         public void deleteUserById(Long id){
--- demo/src/main/java/com/example/demo/repo/UserRepository.java
+++ demo/src/main/java/com/example/demo/repo/UserRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.repo;
 
 import com.example.demo.Models.UserModel;
 import jakarta.transaction.Transactional;
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -18,17 +17,17 @@ public interface UserRepository extends JpaRepository<UserModel, Long>{
     @Transactional
     @Modifying
     @Query(value = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)", nativeQuery = true)
-    int createUserQuery(@Param("name") String name, @Param("email") String email, @Param("password") String password);
+    void createUserQuery(@Param("name") String name, @Param("email") String email, @Param("password") String password);
 
     @Modifying
     @Transactional
     @Query(value = "UPDATE users SET name = :name, email = :email, password = :password WHERE user_id = :id", nativeQuery = true)
-    int editAlluserQuery(@Param("name") String name, @Param("email") String email, @Param("password") String password, @Param("id") Long id);
+    void editAllUserQuery(@Param("name") String name, @Param("email") String email, @Param("password") String password, @Param("id") Long id);
 
     @Modifying
     @Transactional
     @Query(value = "DELETE FROM users WHERE user_id = :id", nativeQuery = true)
-    int deleteUserQuery(@Param("id") Long id);
+    void deleteUserQuery(@Param("id") Long id);
 
 
 }
